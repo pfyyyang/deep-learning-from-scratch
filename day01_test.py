2ea39ab05e9a133de82e17c4e2d527afe6324850
pfyang <pfyyyang@gmail.com>
2026-06-23 09:48:52 +0800

    20260623

---
+++ day01_test.py
@@ -0,0 +1,3 @@
+print("Hello World")
+print("这是我的第一个 Python 学习环境")
+print("我正在使用 python_learning 环境")
---
+++ day01_variables.py
@@ -0,0 +1,11 @@
+name = "Pengfei"
+age = 30
+city = "Verson"
+
+print(name)
+print(age)
+print(city)
+
+print("我的姓名是：", name)
+print("我的年龄是：", age)
+print("我现在所有的城市是：", city)
