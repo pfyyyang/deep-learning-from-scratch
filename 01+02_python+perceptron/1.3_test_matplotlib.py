87a9b01be50273fcadbdb7576ca1dcb9efb63a9c
pfyang <pfyyyang@gmail.com>
2026-06-24 20:58:07 +0800

    python,numpy,perceptron

---
+++ 01+02_python+perceptron/1.3_test_matplotlib.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+x = np.arange(0, 6, 0.1)
+y1 =np. sin(x)
+y2 =np. cos(x)
+
+#绘制图形
+plt.plot(x, y1, label='sin')
+plt.plot(x, y2, linestyle = "--", label = "cos") #用虚线绘制
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Sin & Cos') #标题
+plt.legend()
+plt.show()
--- day01_test.py
+++
@@ -1,3 +0,0 @@
-print("Hello World")
-print("这是我的第一个 Python 学习环境")
-print("我正在使用 python_learning 环境")
